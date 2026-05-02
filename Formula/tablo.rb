927d5c2cbbcc54362f5e7e5b602a651986cc6ec5
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2026-05-02 18:55:18 +0300

    fix description

--- Formula/tablo.rb
+++ Formula/tablo.rb
@@ -1,5 +1,5 @@
 class Tablo < Formula
-  desc "`tablo` is a CLI data pipeline tool that transforms tabular data into formatted ASCII tables"
+  desc "CLI tool that transforms tabular data into formatted ASCII tables"
   homepage "https://github.com/vigo/tablo"
   url "https://github.com/vigo/tablo/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "c086ba5d976121c2a87f8accbd3101e55937d303d0db74a527fae1a635277597"
